f1b2df0f539390160c28b2b78b260fc5cb6e01d8
Rakesh Bhadake <rakeshbhadake185@gmail.com>
2025-01-11 13:57:56 +0530

    Update program3.c

--- program3.c
+++ program3.c
@@ -1,3 +1,17 @@
+/**** 3. Develop a menu driven Program in C for the following operations on STACK of
+ Integers (Array Implementation of Stack with maximum size MAX)
+ a. Push an Element on to Stack
+ b. Pop an Element from Stack
+ c. Demonstrate how Stack can be used to check Palindrome
+ d. Demonstrate Overflow and Underflow situations on Stack
+ e. Display the status of Stack
+ f. Exit
+ Support the program with appropriate functions for each of the above operations****/
+
+
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
