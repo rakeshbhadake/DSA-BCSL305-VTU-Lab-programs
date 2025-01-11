108635951c9d82f95436db4512dc7c44dd2bbac7
Rakesh Bhadake <rakeshbhadake185@gmail.com>
2025-01-11 14:05:30 +0530

    Update program11.c

--- program11.c
+++ program11.c
@@ -1,3 +1,9 @@
+/****11. Develop a Program in C for the following operations on Graph(G) of Cities
+ a. Create a Graph of N cities using Adjacency Matrix.
+ b. Print all the nodes reachable from a given starting node in a digraph using
+ DFS/BFS method****/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
