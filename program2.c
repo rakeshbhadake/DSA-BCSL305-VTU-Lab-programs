897a09dff10f2630d33eae9957fdd35b5face786
Rakesh Bhadake <rakeshbhadake185@gmail.com>
2025-01-11 13:57:04 +0530

    Update program2.c

--- program2.c
+++ program2.c
@@ -1,3 +1,15 @@
+/****
+ 2. Develop a Program in C for the following operations on Strings.
+ a. Read amain String (STR), a Pattern String (PAT) and a Replace String (REP)
+ b. Perform Pattern Matching Operation: Find and Replace all occurrences of PAT in
+ STR with REP if PAT exists in STR. Report suitable messages in case PAT does
+ not exist in STR.
+ Support the program with functions for each of the above operations. Don't use Built-in
+ functions.****/
+
+
+
+
 #include<stdio.h>
  char str[50], pat[20], rep[20], ans[50];
  int c=0, m=0, i=0, j=0, k, flag=0;
